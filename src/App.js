a2891704228b3631c29b82f9dd8db3adf860d96c
tawsifhye <tawsif.lucse@gmail.com>
2021-10-06 16:32:29 +0600

    Implement github login system and display user info

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider} from "firebase/auth";
 import { useState } from "react";
 import './App.css';
 import initializeAuthentication from './Firebase/fireabase.initialize';
 initializeAuthentication();
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
+
 function App() {
   const [user, setUser] = useState({})
+  const auth = getAuth();
   const handlegoogleSignIn = () =>{
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
     .then(result =>{
       const {displayName, email, photoURL} = result.user;
       const loggedInUser= {
@@ -22,12 +24,26 @@ function App() {
       console.log(error.message);
     })
   }
+
+  const handleGitHubSignIn = () =>{
+    signInWithPopup(auth, gitHubProvider)
+    .then(result => {
+      const {displayName, email, photoURL} = result.user;
+      const loggedInUser= {
+        name: displayName,
+        email: email,
+        photo: photoURL
+      };
+      setUser(loggedInUser);
+    })
+  }
   return (
     <div className="App">
       <button onClick={handlegoogleSignIn}>Google Sign in</button>
+      <button onClick={handleGitHubSignIn}>Github Sign In</button>
       <br />
       {
-        user.email && <div>
+        user.name && <div>
           <h2>Welcome {user.name}</h2>
           <p>Email: {user.email}</p>
           <img src={user.photo} alt="" />
--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD-GP8wpDD-a1foZhwSuxaDqo-gZ3aGYYU",
-    authDomain: "simple-firebase-authenti-c969b.firebaseapp.com",
-    projectId: "simple-firebase-authenti-c969b",
-    storageBucket: "simple-firebase-authenti-c969b.appspot.com",
-    messagingSenderId: "1091321046877",
-    appId: "1:1091321046877:web:4cf279868571f6ec6daab4"
+
   };
 
   export default firebaseConfig;
